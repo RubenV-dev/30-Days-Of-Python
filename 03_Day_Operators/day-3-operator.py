673c44fc93d163d189f9130c5ee613266086a5aa
Ruben Vallejo <r.will16@hotmail.com>
2024-07-30 13:52:23 -0400

    day-3-solutions

---
+++ 03_Day_Operators/day-3-operator.py
@@ -0,0 +1,123 @@
+print(True)
+print(False)
+a = 3 # a is a variable name and 3 is an integer data type
+b = 2 # b is a variable name and 3 is an integer data type
+
+# Arithmetic operations and assigning the result to a variable
+total = a + b
+diff = a - b
+product = a * b
+division = a / b
+remainder = a % b
+floor_division = a // b
+exponential = a ** b
+
+# I should have used sum instead of total but sum is a built-in function - try to avoid overriding built-in functions
+print(total) # if you do not label your print with some string, you never know where the result is coming from
+print('a + b = ', total)
+print('a - b = ', diff)
+print('a * b = ', product)
+print('a / b = ', division)
+print('a % b = ', remainder)
+print('a // b = ', floor_division)
+print('a ** b = ', exponential)
+
+# Calculating area of a circle
+radius = 10                                 # radius of a circle
+area_of_circle = 3.14 * radius ** 2         # two * sign means exponent or power
+print('Area of a circle:', area_of_circle)
+
+# Calculating area of a rectangle
+length = 10 # Adding units to these values would add a level of complexity to the concatination 
+width = 20
+area_of_rectangle = length * width
+print('Area of rectangle:', area_of_rectangle)
+
+# Calculating a weight of an object
+mass = 75
+gravity = 9.81
+weight = mass * gravity
+print(weight, 'N')  # Adding unit to the weight
+
+# Calculate the density of a liquid
+mass = 75 # in Kg
+volume = 0.075 # in cubic meter
+density = mass / volume # 1000 Kg/m^3
+
+print(3 > 2)     # True, because 3 is greater than 2
+print(3 >= 2)    # True, because 3 is greater than 2
+print(3 < 2)     # False,  because 3 is greater than 2
+print(2 < 3)     # True, because 2 is less than 3
+print(2 <= 3)    # True, because 2 is less than 3
+print(3 == 2)    # False, because 3 is not equal to 2
+print(3 != 2)    # True, because 3 is not equal to 2
+print(len('mango') == len('avocado'))  # False
+print(len('mango') != len('avocado'))  # True
+print(len('mango') < len('avocado'))   # True
+print(len('milk') != len('meat'))      # False
+print(len('milk') == len('meat'))      # True
+print(len('tomato') == len('potato'))  # True
+print(len('python') > len('dragon'))   # False
+
+
+# Comparing something gives either a True or False
+
+print('True == True: ', True == True)
+print('True == False: ', True == False)
+print('False == False:', False == False)
+
+print('1 is 1', 1 is 1)                   # True - because the data values are the same
+print('1 is not 2', 1 is not 2)           # True - because 1 is not 2
+print('A in Asabeneh', 'A' in 'Asabeneh') # True - A found in the string
+print('B in Asabeneh', 'B' in 'Asabeneh') # False - there is no uppercase B
+print('coding' in 'coding for all') # True - because coding for all has the word coding
+print('a in an:', 'a' in 'an')      # True
+print('4 is 2 ** 2:', 4 is 2 ** 2)   # True
+
+print(3 > 2 and 4 > 3) # True - because both statements are true
+print(3 > 2 and 4 < 3) # False - because the second statement is false
+print(3 < 2 and 4 < 3) # False - because both statements are false
+print('True and True: ', True and True)
+print(3 > 2 or 4 > 3)  # True - because both statements are true
+print(3 > 2 or 4 < 3)  # True - because one of the statements is true
+print(3 < 2 or 4 < 3)  # False - because both statements are false
+print('True or False:', True or False)
+print(not 3 > 2)     # False - because 3 > 2 is true, then not True gives False
+print(not True)      # False - Negation, the not operator turns true to false
+print(not False)     # True
+print(not not True)  # True
+print(not not False) # False
+
+#Exercises
+
+age = 100 # Question1
+height = 6 #Question2
+complex_number = 4j #Question3
+
+#Question4
+base = input('Enter base ')
+triangle_height = input('Enter height ')
+area = float(base) * float(triangle_height)
+print("The area of the triangle is", area)
+
+#Question5
+side_a = input("Enter side a ")
+side_b = input("Enter side b ")
+side_c = input("Enter side c ")
+perimiter = int(side_a) + int(side_b) + int(side_c)
+print("The perimiter of the triangle is ", perimiter)
+
+#Question6
+length_rectangle = input("Length of rectangle ")
+width_rectangle = input("Width of rectangle")
+area_rectangle = int(length_rectangle) * int(width_rectangle)
+perimiter_rectangle = 2 * int(length_rectangle) + int(width_rectangle)
+print("The area of the rectangle is", area_rectangle)
+
+#Question7
+radius_circle = input("What is the radius ")
+pi = 3.14 #float type
+area_cicle = pi * (float(radius_circle) ** 2)
+circumfrence = 2 * pi * float(radius_circle)
+print("The area of the circle is", area_cicle, "The circumfrence of the circle is", circumfrence)
+
